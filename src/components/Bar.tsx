1626182c2dcb87cf6d3f84a74784f8d5362e4bd5
Kamila Nurkhametova <k.nurkhametova@gmail.com>
2026-07-01 18:13:24 +0300

    refactoring: Bar.tsx, FrequencyBars.tsx

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,11 +1,14 @@
+import type { Ref } from 'react'
+import type { Mesh } from 'three'
+
 type BarProps = {
     x: number
-    height: number
+    ref?: Ref<Mesh>
 }
 
-export function Bar({ x, height }: BarProps) {
+export function Bar({ x, ref }: BarProps) {
     return (
-        <mesh position={[x, height / 2, 0]} scale={[1, height, 1]}>
+        <mesh ref={ref} position={[x, 0.5, 0]}>
             <boxGeometry args={[0.25, 1, 0.25]} />
             <meshStandardMaterial color="hotpink" />
         </mesh>
--- src/components/FrequencyBars.tsx
+++ src/components/FrequencyBars.tsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-
+import { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
+import type { Mesh } from 'three'
 
 import { Bar } from './Bar'
 
 export function FrequencyBars() {
     const count = 16
-
-    const [heights, setHeights] = useState<number[]>(
-        Array.from({ length: count }, () => 1)
-    )
+    const barsRef = useRef<(Mesh | null)[]>([])
 
     useFrame(({ clock }) => {
         const time = clock.getElapsedTime()
 
-        const heights = Array.from({ length: count }, (_, idx) => {
+        barsRef.current.forEach((bar, idx) => {
+            if (!bar) {
+                return
+            }
             const x = (idx - count / 2 + 0.5) * 0.4
+            const height = (Math.sin(time + x) + 1.2) * 0.8
 
-            return (Math.sin(time + x) + 1.2) * 0.8
+            bar.scale.y = height
+            bar.position.y = height / 2
         })
-
-        setHeights(heights)
     })
 
     return (
         <group position={[0, -1, 0]}>
-            {heights.map((height, idx) => {
+            {Array.from({ length: count }, (_, idx) => {
                 const x = (idx - count / 2 + 0.5) * 0.4
-                return <Bar key={idx} x={x} height={height} />
+
+                return (
+                    <Bar
+                        key={idx}
+                        x={x}
+                        ref={(mesh) => {
+                            barsRef.current[idx] = mesh
+                        }}
+                    />
+                )
             })}
         </group>
     )
